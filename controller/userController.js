32e0017691efdd7f2e9c86fb4f58d42caf4cc3e0
Mahammad Irshad ap <irshadalike10@gmail.com>
2023-12-05 12:32:51 +0530

    authentication

--- controller/userController.js
+++ controller/userController.js
@@ -1,20 +1,54 @@
 
-
+const { configDotenv } = require('dotenv')
+const userModel = require ('../model/userModel')
+const jwt = require ('jsonwebtoken')
+require('dotenv').config
 const Homes =async(req,res)=>{
     res.send("its working backend server now")
     
 }
 const Register=async(req,res)=>{
-    console.log("cors connected");
-    const {name,email,password,phone}=req.body
-    console.log(name,email,password,phone);
-    
-    res.json({status:"successfull signup"})
+    try {
+        console.log("cors connectedd");
+        const {name,email,phone,password}=req.body
+        console.log(name,email,phone,password);
+        const userExist= await userModel.findOne({email:email})
+        console.log("existemail",userExist);
+        if (userExist) {
+            console.log("User already exists with the email:", email);
+            return res.json({status:"user is alredy exist"});
+        }else{
+        console.log("inserted");
+        const data= await userModel.create({name,email,phone,password})
+        console.log("its env filee-----",process.env.jwtSecretKey);
+        const token = jwt.sign({sub: data._id,Role:data.Role},process.env.jwtSecretKey,{expiresIn: '3d'})
+        console.log(token);
+        console.log(data,"inserted");
+        res.json({token,data, created: true})
+        }
+    } catch (error) {
+        console.error("Error in user registration:", error);
+        res.status(500).json({ error: "Internal server error" });
+    }
 }
 const Logins=async(req,res)=>{
-    console.log("cors connected");
-    const {email,password}=req.body
-    console.log(email,password);
-    res.json({status:"successfull login"})
+    try {
+        console.log("cors connected");
+        const {email,password}=req.body
+        console.log(email,password);
+        const userExist=await userModel.findOne({email:email})
+        console.log(userExist);
+        if(userExist){
+            const token = jwt.sign({sub:userExist._id,Role:userExist.Role},process.env.jwtSecretKey,{expiresIn:"3d"})
+            console.log(token,"-------------");
+            res.json({token,userExist,exist:true})
+        }else{
+            res.json({status:"user not found"})
+            console.log("user not found");
+        }
+    } catch (error) {
+        console.error("Error in user registration:", error);
+        res.status(500).json({ error: "Internal server error" });
+    }
 }
 module.exports ={Homes,Register,Logins}
---
+++ model/userModel.js
@@ -0,0 +1,12 @@
+const mongoose= require('mongoose')
+
+const userSchema = new mongoose.Schema({
+      name: {type:String,require: true},
+      password :{type:String,required:true},
+      email:{type:String},
+      phone:{type:Number},
+      Role:{type:String,default:"User"},            
+        
+})
+
+module.exports = mongoose.model ('user', userSchema)
